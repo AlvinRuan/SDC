2d5fbe35dcb428489d64a06371c20b1963a4a0fa
Alvin Ruan <caskgar@gmail.com>
2025-01-06 23:48:49 -0500

    enabled express urlencoded

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
-// app.use(
-//   express.urlencoded({
-//     extended: true,
-//   })
-// );
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const port = process.env.PORT || 3000;
 
