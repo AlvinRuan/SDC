1ca1c6e22600fccf27800f4756b03a6b0356cb3a
Alvin Ruan <caskgar@gmail.com>
2025-02-19 00:48:22 -0500

    more updating ETL

--- database/dataTransformation.js
+++ database/dataTransformation.js
@@ -6,6 +6,15 @@ fs.readFile(csvFilePath, "utf8", (err, data) => {
   if (err) {
     console.log("Error retreiving csv File", err);
   } else {
-    console.log("Successfully retreived csv File");
+    const current_selection = [];
+
+    const rows = data.split("\n");
+
+    for (let i = 0; i < 10; i++) {
+      console.log(rows[i]);
+      //   current_selection.push(i);
+    }
+
+    // console.log(current_selection);
   }
 });
